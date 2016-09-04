a0aee1709246f18cee9ee81cc233d4f035f08b45
Yongxin Chen <fjcyx01@163.com>
2016-09-04 02:43:45 +0100

    binary

---
+++ binary/main.f90
@@ -0,0 +1,11 @@
+program main
+  implicit none
+  real :: a(10)
+  character :: lf=char(10)
+  open(1, file='new.txt', status = 'replace', form = 'unformatted', access='stream')
+  write(1) a
+  write(1) 'abc',lf
+  write(1) 'def'
+  print*, lf
+  print*, 'abc'//'def'
+end program main
