fa6376863c6d00775fc720f7d4a09084f669e39d
chenyongxin <fjcyx01@163.com>
2016-09-21 21:48:40 +0100

    polymorphism in the same module (*)

--- binary/main.f90
+++ binary/main.f90
@@ -4,7 +4,7 @@ program main
   character :: lf=char(10)
   open(1, file='new.txt', status = 'replace', form = 'unformatted', access='stream')
   write(1) a
-  write(1) 'abc'
+  write(1) 'abc','def',3
   write(1) 'def'
   print*, lf
   print*, 'abc'//'def'
